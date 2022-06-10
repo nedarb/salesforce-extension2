692d7b3619c5e85ae2d6383f1753ee24fe6104d4
nedarb <nedarb@gmail.com>
2022-06-10 13:35:46 -0700

    Clean up code

--- src/scripts/contentscript/index.tsx
+++ src/scripts/contentscript/index.tsx
@@ -14,6 +14,8 @@ interface SObjectDescribeResult {
     type: string;
     updateable: boolean,
     picklistValues?: Array<{active: boolean; label: string; value: string}>
+    relationshipName?: string;
+    referenceTo?: Array<string>;
   }>;
   childRelationships: Array<{
     'cascadeDelete' : boolean;
@@ -91,28 +93,7 @@ function ToDisplay({ cookie, onClose }: {cookie: browser.Cookies.Cookie; onClose
   const sobjectName = extractSObjectName(url);
   const [value2, setMultiSelect] = useState<Array<string>>([]);
   const apiUrl = sobjectName ? `/services/data/v50.0/sobjects/${sobjectName}/describe` : undefined;
-  const { results, isLoading } = useSalesforceApi<SObjectDescribeResult>({ url: apiUrl, cookie, useCache: true });
-
-  const timeoutRef = useRef<number>(-1);
-  const [value, setValue] = useState('');
-  const [loading, setLoading] = useState(false);
-  const [data, setData] = useState<string[]>([]);
-
-  const handleChange = (val: string) => {
-    window.clearTimeout(timeoutRef.current);
-    setValue(val);
-    setData([]);
-
-    if (val.trim().length === 0 || val.includes('@')) {
-      setLoading(false);
-    } else {
-      setLoading(true);
-      timeoutRef.current = window.setTimeout(() => {
-        setLoading(false);
-        setData(['gmail.com', 'outlook.com', 'yahoo.com'].map((provider) => `${val}@${provider}`));
-      }, 1000);
-    }
-  };
+  const { results, isLoading } = useSalesforceApi<SObjectDescribeResult>({ url: apiUrl, cookie, useCache: false });
 
   const options: Array<SelectItem> = useMemo(() => getOptions(value2, results), [value2, results]);
 
@@ -121,20 +102,13 @@ function ToDisplay({ cookie, onClose }: {cookie: browser.Cookies.Cookie; onClose
       HELLO WORLD
       <MultiSelect
         searchable
+        data-autofocus
         value={value2}
         onChange={setMultiSelect}
         data={options}
         label="Your favorite frameworks/libraries"
         placeholder="Pick all that you like"
-      />
-      <Autocomplete
-        data-autofocus
-        value={value}
-        data={data}
-        onChange={handleChange}
-        rightSection={loading ? <Loader size={16} /> : null}
-        label="Async Autocomplete data"
-        placeholder="Your email"
+        rightSection={isLoading ? <Loader size={16} /> : null}
       />
       {JSON.stringify(results)}
     </Modal>
@@ -144,11 +118,6 @@ function ToDisplay({ cookie, onClose }: {cookie: browser.Cookies.Cookie; onClose
 function App() {
   const [isShowing, setIsShowing] = useState(false);
   const handleOverlayClick = () => setIsShowing((cur) => !cur);
-  const timeoutRef = useRef<number>(-1);
-  const inputRef = useRef<HTMLInputElement>();
-  const [value, setValue] = useState('');
-  const [loading, setLoading] = useState(false);
-  const [data, setData] = useState<string[]>([]);
   const [cookie, setCookie] = useState<browser.Cookies.Cookie>();
   useSalesforceApi({ url: '/services/data/v50.0/services/' });
 
@@ -164,40 +133,9 @@ function App() {
     return () => browser.runtime.onMessage.removeListener(listener);
   }, []);
 
-  const handleChange = (val: string) => {
-    window.clearTimeout(timeoutRef.current);
-    setValue(val);
-    setData([]);
-
-    if (val.trim().length === 0 || val.includes('@')) {
-      setLoading(false);
-    } else {
-      setLoading(true);
-      timeoutRef.current = window.setTimeout(() => {
-        setLoading(false);
-        setData(['gmail.com', 'outlook.com', 'yahoo.com'].map((provider) => `${val}@${provider}`));
-      }, 1000);
-    }
-  };
-
   if (!isShowing || !cookie) { return null; }
 
   return <ToDisplay cookie={cookie} onClose={handleOverlayClick} />;
-  // return (
-  //   <Modal title="Salesforce actions" onClose={handleOverlayClick} opened={isShowing}>
-  //     HELLO WORLD
-  //     <Autocomplete
-  //       ref={inputRef as any}
-  //       autoFocus
-  //       value={value}
-  //       data={data}
-  //       onChange={handleChange}
-  //       rightSection={loading ? <Loader size={16} /> : null}
-  //       label="Async Autocomplete data"
-  //       placeholder="Your email"
-  //     />
-  //   </Modal>
-  // );
 }
 
 const el = document.createElement('div');
