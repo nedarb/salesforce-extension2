1c5e72b16a2cd8cf48e2fc93cf4d8198214512aa
nedarb <nedarb@gmail.com>
2022-09-07 15:42:40 -0700

    Add cookie listener

--- src/hooks/useBrowserCookie.ts
+++ src/hooks/useBrowserCookie.ts
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useMemo, useState } from 'react';
 import browser from 'webextension-polyfill';
 
 export interface Params {
@@ -12,14 +12,37 @@ export default function useBrowserCookie({ url, name }: Params): [browser.Cookie
     undefined,
   );
   const [isLoading, setIsLoading] = useState(!!url);
+  const domain = useMemo(() => (url ? new URL(url) : undefined)?.host, [url]);
+  console.debug('domain', domain);
   useEffect(() => {
+    setLastUrl(url);
+
     if (url) {
+      console.log('foobar', url, name);
       setIsLoading(true);
+
       browser.cookies?.get({ url, name }).then((result) => {
         setCookie(result);
       }).finally(() => setIsLoading(false));
+
+      const onChange = (changeInfo: browser.Cookies.OnChangedChangeInfoType) => {
+        const { removed, cookie: changedCookie } = changeInfo;
+        const matchesUrlAndName = changedCookie?.domain === domain && changedCookie?.name === name;
+        if (matchesUrlAndName) {
+          console.log('changed', changeInfo);
+          if (removed) {
+            setCookie(undefined);
+          } else {
+            setCookie(changedCookie);
+          }
+        }
+      };
+      browser.cookies?.onChanged.addListener(onChange);
+
+      return () => browser.cookies?.onChanged.removeListener(onChange);
     }
-    setLastUrl(url);
+
+    return () => {};
   }, [url, name]);
 
   const isActuallyLoading = isLoading || lastUrl !== url;
